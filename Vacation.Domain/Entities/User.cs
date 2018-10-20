7bc6cd776a306a065ef276abe9ca52d1ca159bc2
Pavle Milicevic <milicevic_pz@wvwc.edu>
2018-10-20 00:18:11 -0400

    feat: add user entity

---
+++ Vacation.Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Vacation.Core.Models;
+
+namespace Vacation.Domain.Entities
+{
+    public class User : Entity
+    {
+        public string Email { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        protected User() { }
+
+        public User(Guid id, string email, string firstName, string lastName)
+        {
+            Id = id;
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
